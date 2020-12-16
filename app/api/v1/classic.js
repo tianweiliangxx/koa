33d5ca0217bf53996bd9b36b7827083e5d4706bc
闫海亮 <1015951447@qq.com>
2020-12-16 15:15:35 +0800

    异常监控中间件

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -7,10 +7,17 @@ router.post('/v1/:id/classic/latest', (ctx, next) => {
   const query = ctx.request.query
   const headers = ctx.request.header
   const body = ctx.request.body
+  if (query) {
+    const error = new Error('出现了错误')
+    error.errorCode = 10001
+    error.status = 400
+    error.requestUrl = `${ctx.method} ${ctx.path}`
+    throw error
+  }
   ctx.body = {
     path,
   }
-  throw new Error('API Eception')
+  // throw new Error('API Eception')
 })
 
 module.exports = router
--- middlewares/exception.js
+++ middlewares/exception.js
@@ -2,9 +2,23 @@ const catchError = async (ctx, next) => {
   try {
     await next()
   } catch (error) {
+    if (error.errorCode) {
+      ctx.body = {
+        msg: error.message,
+        error_code: error.errorCode,
+        request: error.requestUrl
+      }
+      ctx.status = error.status
+    }
     // error 堆栈调用信息
-    // error 简化清楚明了的
-    ctx.body = '服务器有问题'
+    // error 简化清楚明了的信息 给前端
+    // HTTP 状态码
+    // message
+    // error_code开发者自定义信息
+    // request_url 当前请求的url
+
+    // 已知错误 try catch
+    // 未知错误 潜在错误 开发者不知道出错
   }
 }
 module.exports = catchError
